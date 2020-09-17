e174f97e87ed2bda5cf10991215bd2392cb76f0d
SujayPrabhu96 <prabhusujay@gmail.com>
2020-09-18 00:06:54 +0530

    Created Login Functionality

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,11 +1,36 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { loginRequest, loginSuccess, loginFailure } from '../actions/loginActions';
+import { login } from '../helpers/Login';
 import { connect } from 'react-redux';
 
 function LoginComponent(props) {
 
+    let history = useHistory();
     const [userData, setUserData] = useState(props.user);
+
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    const handleClick =  async(event) => {
+        event.preventDefault();
+        props.loginRequest(userData);
+        try{
+            const response = await login(userData);
+            if(response.isSuccess){
+                props.loginSuccess(userData);
+                history.push("/home");
+            } else {
+                props.loginFailure(response.message);
+            }
+        } catch(error){
+            props.loginFailure(error);
+        }
+    };
 
     return (
         <div className="container mt-5">
@@ -16,19 +41,19 @@ function LoginComponent(props) {
                 <div className="form-group row">
                     <label htmlFor="email" className="col-2"><strong>Email: </strong></label>
                     <div className="col-4">
-                        <input type="text" className="form-control" name="email" id="email" value={userData.email} />
+                        <input type="text" className="form-control" name="email" id="email" value={userData.email} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="password" className="col-2"><strong>Password: </strong></label>
                     <div className="col-4">
-                        <input type="password" className="form-control" name="password" id="password" value={userData.password} />
+                        <input type="password" className="form-control" name="password" id="password" value={userData.password} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="mb-3">
                     <Link to="/register">Register here</Link>
                 </div>
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary" onClick={handleClick}>Login</button>
             </form>
         </div>
     );
