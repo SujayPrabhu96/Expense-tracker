fce224edb604829bf2d93d1465a304278b31a130
SujayPrabhu96 <prabhusujay@gmail.com>
2020-09-17 23:46:05 +0530

    added mapState and mapDispatch to props in login

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { loginRequest, loginSuccess, loginFailure } from '../actions/loginActions';
+import { connect } from 'react-redux';
 
-function LoginComponent() {
+function LoginComponent(props) {
+
+    const [userData, setUserData] = useState(props.user);
+
     return (
         <div className="container mt-5">
             <form>
@@ -11,13 +16,13 @@ function LoginComponent() {
                 <div className="form-group row">
                     <label htmlFor="email" className="col-2"><strong>Email: </strong></label>
                     <div className="col-4">
-                        <input type="text" className="form-control" name="email" id="email" />
+                        <input type="text" className="form-control" name="email" id="email" value={userData.email}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="password" className="col-2"><strong>Password: </strong></label>
                     <div className="col-4">
-                        <input type="text" className="form-control" name="password" id="password" />
+                        <input type="password" className="form-control" name="password" id="password" value={userData.password}/>
                     </div>
                 </div>
                 <div className="mb-3">
@@ -29,4 +34,18 @@ function LoginComponent() {
     );
 }
 
-export default LoginComponent;
+const mapStateToProps = state => {
+    return{
+        user: state.loginReducer.user
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return{
+        loginRequest: (user) => dispatch(loginRequest(user)),
+        loginSuccess: (user) => dispatch(loginSuccess(user)),
+        loginFailure: (error) => dispatch(loginFailure(error))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginComponent);
