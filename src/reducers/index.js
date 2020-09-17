fd2ecde6a331bf7c2a3e08e904723213293e57bf
SujayPrabhu96 <prabhusujay@gmail.com>
2020-09-17 20:08:37 +0530

    Updated store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import actionReducer from './actionReducer';
 import  registerReducer  from './registerReducer';
 
 const rootReducer = combineReducers({
+    actionReducer,
     registerReducer
 });
 
