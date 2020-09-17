1071e73c67422b3a42ac3703f92e0c53fb1fe410
SujayPrabhu96 <prabhusujay@gmail.com>
2020-09-18 00:06:54 +0530

    Created Login Functionality

--- src/apis/endPoints.js
+++ src/apis/endPoints.js
@@ -1,3 +1,5 @@
 export const apis = {
-    userSignup: '/users/signup'
+    userSignup: '/users/signup',
+    userLogin: "http://localhost:5000/users/login"
+
 };
---
+++ src/helpers/Login.js
@@ -0,0 +1,16 @@
+import { apis } from '../apis/endPoints';
+
+export const login = async({ email, password }) => {
+    try{
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        }
+        return await (await fetch(apis.userLogin, requestOptions)).json();
+    } catch(error){
+        throw new Error(error);
+    }
+}
