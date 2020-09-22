b7cc560cabb3fa028ca1ba2c4b758e24ead3d2f2
SujayPrabhu96 <prabhusujay@gmail.com>
2020-09-22 21:27:18 +0530

    Added Private Route function

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+function PrivateRoute({ component: Component, ...rest }){
+    return(
+        <div>
+            <Route {...rest} render={(props) => {
+                if(!localStorage.getItem('user')){
+                   return <Redirect to="/login" />
+                }
+                return <Component {...props} />
+            }
+            }></Route>
+        </div>
+    );
+}
+
+export default PrivateRoute;
