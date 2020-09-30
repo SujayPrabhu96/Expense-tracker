25b98bc814fffa2bbd028a526f0788a61b57615e
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-01 00:14:33 +0530

    updated Login with axios

--- src/helpers/Login.js
+++ src/helpers/Login.js
@@ -1,18 +1,7 @@
 import { apis } from '../apis/endPoints';
+import axios from '../apis/axios';
 
 export const login = async({ email, password }) => {
-    try{
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        }
-        const user = await (await fetch(apis.userLogin, requestOptions)).json();
-        localStorage.setItem('user', JSON.stringify(user));
-        return user;
-    } catch(error){
-        throw new Error(error);
-    }
+    const response = await axios.post(apis.userLogin, { email, password });
+    return response.data;
 }
