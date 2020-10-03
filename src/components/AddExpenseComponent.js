f4aaa6fec0e4d31ba0d7a630d3c93b409ecdde71
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-03 23:51:15 +0530

    updated add expense component with hooks to handle onChange event

--- src/components/AddExpenseComponent.js
+++ src/components/AddExpenseComponent.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeExpenseInput } from '../actions/saveExpenseActions';
 
 function AddExpenseComponent(){
+
+    const expense = useSelector(state => state.saveExpenseReducer);
+    const dispatch = useDispatch();
+
+    const handleChange = (event) => {
+        dispatch(changeExpenseInput(event.target.name, event.target.value));
+    }
 
     return(
         <div>
@@ -10,19 +19,19 @@ function AddExpenseComponent(){
                 <div className="form-group row">
                     <label htmlFor="date" className="col-2"><strong>Date: </strong></label>
                     <div className="col-4">
-                        <input type="text" name="date" id="date" className="form-control datepicker"/>
+                        <input type="text" name="date" id="date" className="form-control" value={expense.date} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="amount" className="col-2"><strong>Amount: </strong></label>
                     <div className="col-4">
-                        <input type="number" name="amount" id="amount" className="form-control"/>
+                        <input type="number" name="amount" id="amount" className="form-control" value={expense.amount} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="description" className="col-2"><strong>Description: </strong></label>
                     <div className="col-4">
-                        <input type="text" name="description" id="description" className="form-control"/>
+                        <input type="text" name="description" id="description" className="form-control" value={expense.description} onChange={handleChange}/>
                     </div>
                 </div>
                 <div>
