c11c16f9b016107808a2561b24784f1f753e88af
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-06 21:13:59 +0530

    try catch with async

--- src/helpers/Login.js
+++ src/helpers/Login.js
@@ -2,9 +2,14 @@ import { apis } from '../apis/endPoints';
 import axios from '../apis/axios';
 
 export const login = async({ email, password }) => {
-    const response = await axios.post(apis.userLogin, { email, password });
-    localStorage.setItem('user', JSON.stringify(response.data));
-    return response.data;
+    try{
+        const response = await axios.post(apis.userLogin, { email, password });
+        localStorage.setItem('user', JSON.stringify(response.data));
+        return response.data;
+    } catch(error){
+        throw new Error(error);
+    }
+    
 }
 
 export const checkIfUserLoggedIn = () => {
