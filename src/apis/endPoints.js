dfb8fded2fe5bb66a2d3a3ac7f80a882b791b6ab
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-07 20:32:58 +0530

    added delete endpoints and perform delete with axios

--- src/apis/endPoints.js
+++ src/apis/endPoints.js
@@ -2,5 +2,6 @@ export const apis = {
     userSignup: "/users/signup",
     userLogin: "/users/login",
     userExpenses: "/users/expenses",
-    saveExpense: "/users/save-expense"
+    saveExpense: "/users/save-expense",
+    deleteExpense: "/users/delete-expense/"
 };
--- src/helpers/Expenses.js
+++ src/helpers/Expenses.js
@@ -18,3 +18,12 @@ export const saveExpense = async (expense) => {
         throw new Error(error);
     }
 }
+
+export const deleteExpense = async (id) => {
+    try{
+        const delete_url = `${apis.deleteExpense}${id}`;
+        const response = axios.delete(delete_url);
+    } catch(error){
+        throw new Error(error);
+    }
+}
