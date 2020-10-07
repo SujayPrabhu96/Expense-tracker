a215536d5f50d712f7c9c32c8e5527f1d871a4b1
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-07 21:02:17 +0530

    added delete expense reducer

---
+++ src/reducers/deleteExpenseReducer.js
@@ -0,0 +1,33 @@
+import { deleteExpenseConstants } from '../constants/actionTypes';
+import { act } from 'react-dom/test-utils';
+
+const initialState = {
+    loading: false,
+    error: ''
+};
+
+const deleteExpenseReducer = (state = initialState, action) => {
+    switch(action.type){
+        case deleteExpenseConstants.DELETE_EXPENSE_REQUEST:
+            return{
+                ...state,
+                loading: true,
+                expense_id: action.payload
+            }
+        case deleteExpenseConstants.DELETE_EXPENSE_SUCCESS:
+            return{
+                ...state,
+                loading: false
+            }
+        case deleteExpenseConstants.DELETE_EXPENSE_FAILURE:
+            return{
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+};
+
+export default deleteExpenseReducer;
