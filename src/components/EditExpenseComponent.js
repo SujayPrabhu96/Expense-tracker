837db63c119e9a06f5682d202785caae914bf2b2
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-08 23:40:36 +0530

    displaying data to be edited in input fields

--- src/components/EditExpenseComponent.js
+++ src/components/EditExpenseComponent.js
@@ -7,18 +7,20 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 function EditExpenseComponent(){
 
-    const expense = useSelector(state => state.saveExpenseReducer);
+    const expenseData = useSelector(state => state.getExpenseReducer);
 
     return(
         <div>
+            {expenseData.loading ? <h2>Loading...</h2> :
+            expenseData.expense && expenseData.expense.data &&
             <form>
-                <h1 className="mb-5">Add Expense</h1>
+                <h1 className="mb-5">Edit Expense</h1>
                 <div className="form-group row">
                     <label htmlFor="date" className="col-2"><strong>Date: </strong></label>
                     <div className="col-4">
                         <DatePicker 
                             className="form-control" 
-                            selected={expense.date && moment(expense.date, "YYYY-MM-DD").toDate()} 
+                            selected={expenseData.expense.data.date && moment(expenseData.expense.data.date, "YYYY-MM-DD").toDate()} 
                         >
                         </DatePicker>
                     </div>
@@ -26,13 +28,13 @@ function EditExpenseComponent(){
                 <div className="form-group row">
                     <label htmlFor="amount" className="col-2"><strong>Amount: </strong></label>
                     <div className="col-4">
-                        <input type="number" name="amount" id="amount" className="form-control" value={expense.amount}/>
+                        <input type="number" name="amount" id="amount" className="form-control" value={expenseData.expense.data.amount}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="description" className="col-2"><strong>Description: </strong></label>
                     <div className="col-4">
-                        <input type="text" name="description" id="description" className="form-control" value={expense.description}/>
+                        <input type="text" name="description" id="description" className="form-control" value={expenseData.expense.data.description}/>
                     </div>
                 </div>
                 <div>
@@ -40,6 +42,7 @@ function EditExpenseComponent(){
                     <Link to="/" className="btn btn-danger delete">Cancel</Link>
                 </div>
             </form>
+        }
         </div>
     );
 }
