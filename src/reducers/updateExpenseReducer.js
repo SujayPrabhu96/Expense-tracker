cc8a03389780d3a70cadfa64c8daf6aa63a099af
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-09 20:17:15 +0530

    added update expense reducer

---
+++ src/reducers/updateExpenseReducer.js
@@ -0,0 +1,37 @@
+import { updateExpenseConstants } from '../constants/actionTypes';
+
+const initialState = {
+    loading: false,
+    error: '',
+    btnDisabled: false
+}
+
+const updateExpenseReducer = (state = initialState, action) => {
+    switch(action.type){
+        case updateExpenseConstants.UPDATE_EXPENSE_REQUEST:
+            return{
+                ...state,
+                loading: true,
+                btnDisabled: true,
+                expense: action.payload
+            }
+        case updateExpenseConstants.UPDATE_EXPENSE_SUCCESS:
+            return{
+                ...state,
+                loading: false,
+                btnDisabled: false,
+                response: action.payload
+            }
+        case updateExpenseConstants.UPDATE_EXPENSE_FAILURE:
+            return{
+                ...state,
+                loading: false,
+                btnDisabled: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+};
+
+export default updateExpenseReducer;
