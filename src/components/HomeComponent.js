18e0fcf66a20fda2df1284e9567ffa28d18e50e2
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-14 19:55:19 +0530

    removed unnecessary imports in Home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getUserExpenses, getUserExpense } from '../actions/getExpensesAction';
-import { setError } from '../actions/alertActions';
-import { handleDeleteExpense } from '../actions/deleteExpenseAction';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function HomeComponent() {
