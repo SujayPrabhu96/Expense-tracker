63682cb79b3ef9d8710bf855b487f58d8ab9f49d
SujayPrabhu96 <prabhusujay@gmail.com>
2020-10-16 20:55:24 +0530

    updated add expense component variable

--- src/helpers/Expenses.js
+++ src/helpers/Expenses.js
@@ -4,7 +4,7 @@ import axios from '../apis/axios';
 export const listExpenses = async () => {
     try{
         const response = await axios.get(apis.userExpenses);
-        return response.data;
+        return response.data.data;
     } catch(error){
         throw new Error(error);
     }
